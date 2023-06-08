ac7a44e769f1ec4a0249ec721f189bbf3bcd51f5
mhmdnurf <mhmd.nurfat@gmail.com>
2023-06-09 04:54:38 +0700

    add put route on reminder and report

--- routes/reminders.js
+++ routes/reminders.js
@@ -29,7 +29,7 @@ router.get("/", auth, async (req, res) => {
     const reminders = await RemindersModel.findAll();
 
     res.status(200).json({
-      reminders: reminders,
+      data: reminders,
     });
   } catch (error) {
     res.status(500).json({
@@ -65,7 +65,7 @@ router.post("/create", auth, upload.single("gambar"), async (req, res) => {
     stream.on("finish", async () => {
       try {
         // Membuat data reminder dengan informasi gambar dari Google Cloud Storage
-        const reminder = await RemindersModel.create({
+        const createReminder = await RemindersModel.create({
           user_id,
           tanggal_awal,
           tanggal_akhir,
@@ -76,7 +76,7 @@ router.post("/create", auth, upload.single("gambar"), async (req, res) => {
 
         res.status(200).json({
           message: "Data reminder berhasil diinput",
-          data: reminder,
+          data: createReminder,
         });
       } catch (error) {
         console.error("Error saat membuat data reminder:", error);
@@ -101,16 +101,16 @@ router.get("/detail/:reminderId", auth, async (req, res) => {
     const reminderId = req.params.reminderId;
 
     // Mengambil data report berdasarkan user_id dan report_id
-    const data = await RemindersModel.findOne({
+    const detailReminder = await RemindersModel.findOne({
       where: { user_id: userId, id: reminderId },
     });
 
-    if (!data) {
+    if (!detailReminder) {
       return res.status(404).json({ error: "Report not found" });
     }
 
     res.status(200).json({
-      data: data,
+      data: detailReminder,
     });
   } catch (error) {
     res
@@ -119,9 +119,91 @@ router.get("/detail/:reminderId", auth, async (req, res) => {
   }
 });
 
+router.put("/edit/:reminderId", auth, upload.single("gambar"), async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const reminderId = req.params.reminderId;
+    const { tanggal_awal, tanggal_akhir, lokasi, catatan } = req.body;
+    let gambar = req.file; // Mengambil informasi file gambar yang diunggah
+
+    // Mengecek apakah ada file gambar baru yang diunggah
+    if (gambar) {
+      // Menentukan nama file di Google Cloud Storage
+      const namaFile = Date.now() + "-" + gambar.originalname;
+
+      // Mengunggah file ke Google Cloud Storage
+      const file = bucket.file(namaFile);
+      const stream = file.createWriteStream({
+        metadata: {
+          contentType: gambar.mimetype,
+        },
+      });
+
+      stream.on("error", (error) => {
+        console.error("Error saat mengunggah file:", error);
+        res.status(500).json({
+          error: "Data gagal diupdate",
+        });
+      });
+
+      stream.on("finish", async () => {
+        try {
+          // Mengupdate data reminder dengan informasi gambar baru dari Google Cloud Storage
+          const updatedReminder = await RemindersModel.update(
+            {
+              tanggal_awal,
+              tanggal_akhir,
+              gambar: `https://storage.googleapis.com/${bucket.name}/${namaFile}`,
+              lokasi,
+              catatan,
+            },
+            {
+              where: { user_id: userId, id: reminderId },
+            }
+          );
+
+          res.status(200).json({
+            message: "Data reminder berhasil diupdate",
+          });
+        } catch (error) {
+          console.error("Error saat mengupdate data reminder:", error);
+          res.status(500).json({
+            error: "Data gagal diupdate",
+          });
+        }
+      });
+
+      stream.end(gambar.buffer);
+    } else {
+      // Jika tidak ada gambar baru yang diunggah, hanya mengupdate data reminder tanpa mengubah gambar
+      const updatedReminder = await RemindersModel.update(
+        {
+          tanggal_awal,
+          tanggal_akhir,
+          lokasi,
+          catatan,
+        },
+        {
+          where: { user_id: userId, id: reminderId },
+        }
+      );
+
+      res.status(200).json({
+        message: "Data reminder berhasil diupdate",
+        data: updatedReminder,
+      });
+    }
+  } catch (error) {
+    console.error("Error saat mengolah data:", error);
+    res.status(500).json({
+      error: "Terjadi kesalahan",
+    });
+  }
+});
+
 router.get("/search", auth, async (req, res) => {
   try {
-    const reminder = await RemindersModel.findAll({
+    const searchReminder = await RemindersModel.findAll({
       where: {
         [Sequelize.Op.or]: [
           { lokasi: { [Sequelize.Op.like]: `%${req.query.search}%` } },
@@ -129,9 +211,9 @@ router.get("/search", auth, async (req, res) => {
         ],
       },
     });
-    if (reminder.length > 0) {
+    if (searchReminder.length > 0) {
       res.status(200).json({
-        data: reminder,
+        data: searchReminder,
       });
     } else {
       res.status(200).json({
