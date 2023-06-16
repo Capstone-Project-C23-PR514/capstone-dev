54befa18eceb14e5387bed8f9b1c329dfdfb8aa5
mhmdnurf <mhmd.nurfat@gmail.com>
2023-06-16 17:42:11 +0700

    fix cloud storafe

--- routes/reminders.js
+++ routes/reminders.js
@@ -20,9 +20,9 @@ const upload = multer({ storage: storage });
 // Buat instance Google Cloud Storage
 const storageClient = new Storage({
   projectId: process.env.GCS_PROJECT_ID,
-  keyFilename: "lippo-capstone.json", // Ganti dengan path yang benar ke file kredensial Anda
+  keyFilename: "road-crack.json", // Ganti dengan path yang benar ke file kredensial Anda
 });
-const bucket = storageClient.bucket("road-crack-model"); // Ganti dengan nama bucket Anda
+const bucket = storageClient.bucket("road-crack-storage"); // Ganti dengan nama bucket Anda
 
 router.get("/", auth, async (req, res) => {
   try {
@@ -119,87 +119,92 @@ router.get("/detail/:reminderId", auth, async (req, res) => {
   }
 });
 
-router.put("/edit/:reminderId", auth, upload.single("gambar"), async (req, res) => {
-  try {
-    const userId = req.user.id;
-    const reminderId = req.params.reminderId;
-    const { tanggal_awal, tanggal_akhir, lokasi, catatan } = req.body;
-    let gambar = req.file; // Mengambil informasi file gambar yang diunggah
+router.put(
+  "/edit/:reminderId",
+  auth,
+  upload.single("gambar"),
+  async (req, res) => {
+    try {
+      const userId = req.user.id;
+      const reminderId = req.params.reminderId;
+      const { tanggal_awal, tanggal_akhir, lokasi, catatan } = req.body;
+      let gambar = req.file; // Mengambil informasi file gambar yang diunggah
 
-    // Mengecek apakah ada file gambar baru yang diunggah
-    if (gambar) {
-      // Menentukan nama file di Google Cloud Storage
-      const namaFile = Date.now() + "-" + gambar.originalname;
+      // Mengecek apakah ada file gambar baru yang diunggah
+      if (gambar) {
+        // Menentukan nama file di Google Cloud Storage
+        const namaFile = Date.now() + "-" + gambar.originalname;
 
-      // Mengunggah file ke Google Cloud Storage
-      const file = bucket.file(namaFile);
-      const stream = file.createWriteStream({
-        metadata: {
-          contentType: gambar.mimetype,
-        },
-      });
-
-      stream.on("error", (error) => {
-        console.error("Error saat mengunggah file:", error);
-        res.status(500).json({
-          error: "Data gagal diupdate",
+        // Mengunggah file ke Google Cloud Storage
+        const file = bucket.file(namaFile);
+        const stream = file.createWriteStream({
+          metadata: {
+            contentType: gambar.mimetype,
+          },
         });
-      });
 
-      stream.on("finish", async () => {
-        try {
-          // Mengupdate data reminder dengan informasi gambar baru dari Google Cloud Storage
-          const updatedReminder = await RemindersModel.update(
-            {
-              tanggal_awal,
-              tanggal_akhir,
-              gambar: `https://storage.googleapis.com/${bucket.name}/${namaFile}`,
-              lokasi,
-              catatan,
-            },
-            {
-              where: { user_id: userId, id: reminderId },
-            }
-          );
-
-          res.status(200).json({
-            message: "Data reminder berhasil diupdate",
-          });
-        } catch (error) {
-          console.error("Error saat mengupdate data reminder:", error);
+        stream.on("error", (error) => {
+          console.error("Error saat mengunggah file:", error);
           res.status(500).json({
             error: "Data gagal diupdate",
           });
-        }
-      });
+        });
 
-      stream.end(gambar.buffer);
-    } else {
-      // Jika tidak ada gambar baru yang diunggah, hanya mengupdate data reminder tanpa mengubah gambar
-      const updatedReminder = await RemindersModel.update(
-        {
-          tanggal_awal,
-          tanggal_akhir,
-          lokasi,
-          catatan,
-        },
-        {
-          where: { user_id: userId, id: reminderId },
-        }
-      );
+        stream.on("finish", async () => {
+          try {
+            // Mengupdate data reminder dengan informasi gambar baru dari Google Cloud Storage
+            const updatedReminder = await RemindersModel.update(
+              {
+                tanggal_awal,
+                tanggal_akhir,
+                gambar: `https://storage.googleapis.com/${bucket.name}/${namaFile}`,
+                lokasi,
+                catatan,
+              },
+              {
+                where: { user_id: userId, id: reminderId },
+              }
+            );
 
-      res.status(200).json({
-        message: "Data reminder berhasil diupdate",
-        data: updatedReminder,
+            res.status(200).json({
+              message: "Data reminder berhasil diupdate",
+            });
+          } catch (error) {
+            console.error("Error saat mengupdate data reminder:", error);
+            res.status(500).json({
+              error: "Data gagal diupdate",
+            });
+          }
+        });
+
+        stream.end(gambar.buffer);
+      } else {
+        // Jika tidak ada gambar baru yang diunggah, hanya mengupdate data reminder tanpa mengubah gambar
+        const updatedReminder = await RemindersModel.update(
+          {
+            tanggal_awal,
+            tanggal_akhir,
+            lokasi,
+            catatan,
+          },
+          {
+            where: { user_id: userId, id: reminderId },
+          }
+        );
+
+        res.status(200).json({
+          message: "Data reminder berhasil diupdate",
+          data: updatedReminder,
+        });
+      }
+    } catch (error) {
+      console.error("Error saat mengolah data:", error);
+      res.status(500).json({
+        error: "Terjadi kesalahan",
       });
     }
-  } catch (error) {
-    console.error("Error saat mengolah data:", error);
-    res.status(500).json({
-      error: "Terjadi kesalahan",
-    });
   }
-});
+);
 
 router.get("/search", auth, async (req, res) => {
   try {
@@ -248,6 +253,5 @@ router.delete("/delete/:reminderId", auth, async (req, res) => {
     res.status(500).json({ error: "Failed to delete reminder" });
   }
 });
-
 
 module.exports = router;
